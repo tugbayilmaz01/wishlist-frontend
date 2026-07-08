87300fa6c8436cd2910aaf09915121d147bad3aa
Tuğba Yılmaz <tugbayill01@gmail.com>
2026-07-08 15:00:09 +0300

    added API pre-warming on initial load

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,14 +1,19 @@
 "use client";
 
+import { useEffect } from "react";
 import { SessionProvider } from "next-auth/react";
 import { LanguageProvider } from "@/context/LanguageContext";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    if (process.env.NEXT_PUBLIC_API_URL) {
+      fetch(`${process.env.NEXT_PUBLIC_API_URL}/wishlists`).catch(() => {});
+    }
+  }, []);
+
   return (
     <SessionProvider>
-      <LanguageProvider>
-        {children}
-      </LanguageProvider>
+      <LanguageProvider>{children}</LanguageProvider>
     </SessionProvider>
   );
 }
